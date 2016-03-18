fffb8d5a5d45523180b2cedb1c3c139ba67839f0
guopengb@chanjet.com <guopengb@chanjet.com>
2016-03-18 16:13:28 +0800

    init.

---
+++ generators/app/templates/src/js/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created with WebStorm.
+ * User: gp
+ * Date: 16/3/18
+ *
+ */
+
+import React from 'react';
+import ReactDOM, {render} from 'react-dom';
+
+import '../css/main.css'
+
+
+render(
+    <div>
+        <h1 style={{"textAlign":"center"}}>welcome es6 world!</h1>
+    </div>,
+    document.getElementById('app')
+)
